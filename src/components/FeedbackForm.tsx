43534f2c5881bdffb116beeac04080d785e50da7
debojit59 <debojitbanerjee59@gmail.com>
2026-06-06 22:55:25 +0530

    fix: update page

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
 
 export default function FeedbackForm() {
+
+  const [text,setText] = useState('');
+
+
+  const HandleValueChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value)}
+
+  const MaxLength = 150
+  const CharLength = MaxLength- text.length ;
+
   return (
     <form className="form">
-      <textarea id="feedback-form" placeholder="bla bla"/>
+      <textarea id="feedback-form" placeholder="bla bla" 
+      value ={text}
+      onChange={HandleValueChange}
+      maxLength={MaxLength}/>
       <label htmlFor="feedback-form"> 
         Share your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{CharLength}</p>
         <button>
           <span>
             submit
